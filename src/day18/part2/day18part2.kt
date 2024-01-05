9e7479765a8d16d4ade888946e839fc1dd9338a4
alexbft <alexbft@users.noreply.github.com>
2024-01-05 12:51:56 +0100

    day 18

---
+++ src/day18/part2/day18part2.kt
@@ -0,0 +1,22 @@
+package day18.part2
+
+import bootstrap.readLines
+
+fun solve(firstRow: String, rowsCount: Int): Int {
+    var row = firstRow.map { it == '^' }
+    var total = 0
+    for (rowIndex in 2..rowsCount + 1) {
+        total += row.count { !it }
+        row = row.indices.map { x ->
+            val left = if (x == 0) false else row[x - 1]
+            val right = if (x == row.size - 1) false else row[x + 1]
+            left != right
+        }
+    }
+    return total
+}
+
+fun main() {
+    val input = readLines("day18.txt").first()
+    println(solve(input, 400000))
+}
---
+++ test/day18/part1/Day18part1KtTest.kt
@@ -0,0 +1,13 @@
+package day18.part1
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day18part1KtTest {
+
+    @Test
+    fun solve() {
+        assertEquals(38, solve(".^^.^.^^^^", 10))
+    }
+}
